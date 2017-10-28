3584f04a57131a1d4f30d0f0e2afbe08d615700d
Paulo Abner <pauloabner@gmail.com>
2017-10-28 17:49:06 -0300

    Quiz menu

--- src/jogos/Quiz.js
+++ src/jogos/Quiz.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
+import Card, { CardActions, CardContent } from 'material-ui/Card';
+import Button from 'material-ui/Button';
+import Typography from 'material-ui/Typography';
+
 
 export default class Quiz extends Component {
   render() {
+
+
     return (
       <div>
-        <h1>Quiz</h1>
-
+        <Card>
+          <CardContent>
+            <Typography type="headline" component="h2">
+              Tema: Bandas de Rock
+            </Typography>
+            <Typography type="body1">
+              Início: 19 Horas<br />
+              Prêmio: Chopp grátis
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button raised color="primary">Participar</Button>
+          </CardActions>
+        </Card>
+        <br/>
+        <Card>
+          <CardContent>
+            <Typography type="headline" component="h2">
+              Tema: Séries de TV
+            </Typography>
+            <Typography type="body1">
+              Início: 20 Horas<br />
+              Prêmio: Porção Spicy Wings
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button raised color="primary">Participar</Button>
+          </CardActions>
+        </Card>
       </div>
     );
   }
 }
+
+
