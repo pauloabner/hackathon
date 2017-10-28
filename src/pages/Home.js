9bc844f85bef5694d71f5c4f1e4c000116302cd7
Henrique Gogó <henriquegogo@gmail.com>
2017-10-28 12:42:57 -0300

    Tela de procurando estabelecimento

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { CircularProgress } from 'material-ui/Progress';
 
 export default class Home extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Home</h1>
-        <p>Welcome home!</p>
-        <Link to='/estabelecimento'>Estabelecimento</Link>
+      <div style={{position: 'fixed',
+                   top: 0, bottom: 0, left: 0, right: 0,
+                   textAlign: 'center', background: '#9ed4ec'}}>
+        <br/>
+        <br/>
+        <br/>
+        <Link to='/estabelecimento'>
+          <img src='/images/local.png' style={{maxWidth: '100%'}} />
+        </Link>
+        <br/>
+        <CircularProgress color="accent" />
+        <br/>
+        <br/>
+        <div style={{fontSize: '13px'}}>Procurando estabelecimento...</div>
       </div>
     );
   }
