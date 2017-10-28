93e1b072372242b1bb526c424bde46e16b867727
Rodrigo Araujo <rodrigo.araujo2@hp.com>
2017-10-28 17:37:03 -0300

    novos jogos.

--- src/pages/Jogos.js
+++ src/pages/Jogos.js
@@ -37,6 +37,18 @@ export default class Jogos extends Component {
               <GridListTileBar title='Teste de Fidelidade' subtitle='Você vem sempre aqui?' />
             </Link>
           </GridListTile>
+          <GridListTile key={'karaoke'}>
+            <img src='/images/karaoke.png' alt='Karaoke' />
+            <Link to='/jogos/karaoke'>
+              <GridListTileBar title='Jurados!' subtitle='Você avalia os nossos cantores amadores.' />
+            </Link>
+          </GridListTile>
+          <GridListTile key={'batalha'}>
+            <img src='/images/batalha.jpg' alt='Batalha entre Mesas' />
+            <Link to='/jogos/batalha'>
+              <GridListTileBar title='Batalha entre mesas!' subtitle='Chame uma mesa para uma batalha.' />
+            </Link>
+          </GridListTile>
         </GridList>
       </div>
     );
