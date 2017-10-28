d4d19980a22aeee3f7547f611a8264f6543b4246
Paulo Abner <pauloabner@gmail.com>
2017-10-28 16:10:44 -0300

    imagem da roleta

--- src/jogos/Roleta.js
+++ src/jogos/Roleta.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import Button from 'material-ui/Button';
+
+const style = {
+  margin: 12,
+};
 
 export default class Roleta extends Component {
-
   render() {
     return (
       <div>
         <h1>Roleta</h1>
-        <p>Rode a roda!</p>
+        <div >
+          <img src='/images/wheel.png' style={{ maxWidth: '100%' }} />
+        </div>
+        <Button raised color="primary" style={style} > Rodar </Button>
       </div>
     );
   }
